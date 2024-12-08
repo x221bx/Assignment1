6ada7ffda8fa303d484797b539402f76639f4a7e
x221bx <ahmad1zain10@gmail.com>
2024-12-08 15:08:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.assignment1;
+
+import static com.example.assignment1.R.id.main;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.facebook_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
